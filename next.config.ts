bb7ddfb0f5a67b3727c0ecec0b7431ad9f450f82
Oleksii Mazurenko <101861681+oleksiimazurenko@users.noreply.github.com>
2025-03-23 18:30:04 +0200

    fix: add remotePatterns for image cdn

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,10 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
       },
+      {
+        protocol: "https",
+        hostname: "promova-test-backend.onrender.com",
+      },
     ],
   },
   experimental: {
